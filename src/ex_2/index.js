783b0c6c3c44a23bf5ce04aa579e8699280fc538
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-02-28 22:42:32 -0300

    aula 28-02

---
+++ src/ex_2/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'; 
+
+export default function Mensagem () {
+    return(
+        <View>
+            <Text>Hello React-Native</Text>
+        </View>
+    );
+}
