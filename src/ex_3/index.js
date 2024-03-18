ed429d8063e59d0b67cc90c4c9f025f25fc437aa
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-03-18 20:41:37 -0300

    aula 18-03

--- src/ex_3/index.js
+++ src/ex_3/index.js
@@ -1,9 +1,22 @@
-import { View, Text, Pressable } from 'react-native'; 
+import { View, Text, Pressable, TouchableHighlight, TouchableOpacity } from 'react-native';
 
-export default function Exemplo_3 () {
-    return(
+export default function Exemplo_3() {
+    return (
         <View>
             <Text>Exemplo 3</Text>
+            <Pressable style={({ pressed }) => [
+                {
+                    backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
+                },
+            ]}>
+                <Text>Botão</Text>
+            </Pressable>
+            <TouchableHighlight>
+                <Text>Botão Highlight</Text>
+            </TouchableHighlight>
+            <TouchableOpacity>
+                <Text>Botão Opacit</Text>
+            </TouchableOpacity>
         </View>
-    ); 
+    );
 }
