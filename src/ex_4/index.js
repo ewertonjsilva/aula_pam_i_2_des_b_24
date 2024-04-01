40ff2105d02bdc355010256d78c14a8f689ac3ac
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-04-01 20:42:09 -0300

    aula 01-04

--- src/ex_4/index.js
+++ src/ex_4/index.js
@@ -1,17 +1,44 @@
-import { View, Text, TextInput } from 'react-native'; 
+import { useState } from 'react';
+import { View, Text, TextInput, Pressable } from 'react-native'; 
 
 import styles from './styles';
 
-export default function Exemplo4 () {
+export default function Exemplo4 () { 
+
+    const [txtDigitado, setTxtDigitado] = useState('Texto digitado'); 
+    const [txt2, setTxt2] = useState('');
+    const [txtTela, setTxtTela] = useState('');
+
+    function atualizaTextoHandle (txt) {
+        setTxtDigitado(txt);
+    }
+
     return(
         <View style={styles.container}>
             <Text style={styles.titulo}>Exemplo 4</Text> 
 
-            <Text style={styles.texto}>Texto</Text> 
+            <Text style={styles.texto}>{txtDigitado}</Text> 
             
-            <TextInput style={styles.input} />
+            <TextInput 
+                style={styles.input} 
+                onChangeText={(valor) => atualizaTextoHandle(valor)}
+            /> 
 
+            <View style={styles.linha} /> 
+
+            <Text style={styles.texto}>{txtTela}</Text> 
             
+            <TextInput 
+                style={styles.input} 
+                onChangeText={(vlr) => setTxt2(vlr)} 
+                value={txt2}
+            />
+
+            <Pressable 
+                style={({ pressed }) => pressed ? [styles.botao, styles.botaoPress] : styles.botao}
+            >
+                <Text style={styles.txtBotao}>Exibir texto</Text>
+            </Pressable>
         </View>
     );
 }
--- src/ex_4/styles.js
+++ src/ex_4/styles.js
@@ -30,6 +30,30 @@ const styles = StyleSheet.create({
         borderRadius: 20, 
         padding: 5, 
     }, 
+    linha: { 
+        borderBottomWidth: 3, 
+        borderColor: '#b71c1c', 
+        marginVertical: 20, 
+        width: '100%'
+    }, 
+    botao: {
+        width: '80%', 
+        padding: 16, 
+        alignItems: 'center',  
+        borderRadius: 20, 
+        backgroundColor: '#b71c1c', 
+        marginVertical: 20, 
+    }, 
+    botaoPress: {
+        backgroundColor: '#fff', 
+    }, 
+    txtBotao: {
+        color: '#ffcdd2', 
+        fontSize: 20,         
+    },
+    txtBotaoPress: {
+        color: '#b71c1c',       
+    }, 
 }); 
 
 export default styles;
